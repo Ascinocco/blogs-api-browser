e4cdc3b2d33685b0d76ba30ea142fdfa844b0241
Anthony Scinocco <scinocco.a@gmail.com>
2019-03-25 16:20:18 -0400

    added default case for detail view

--- src/components/DetailView/index.js
+++ src/components/DetailView/index.js
@@ -18,62 +18,71 @@ const defaultProps = {
 
 
 
-const renderEntries = (entries) => entries.map((entry) => {
-  const { author, attributes, comments } = entry
-
-  const authorDetails = (
-    <div>
-      <h5>Author</h5>
-      <ul>
-        <li>{author.attributes.name}: {author.attributes.email}</li>
-      </ul>
-    </div>
-  )
-
-  const attributeItems = Object.keys(attributes).map(key =>(
-    <li key={`entry-${entry.id}-key-${key}`}>
-      {key}: {entry.attributes[key]}
-    </li>
-  ))
-
-  const entryAttributes = (
-    <div>
-      <h5>Details</h5>
-      <ul>
-        {attributeItems}
-      </ul>
-    </div>
-  )
-
-  const commentItems = comments.map((comment) => {
-    const { commentWriter } = comment
-    console.log('COMMENT', comment)
-    const name = !isEmpty(commentWriter) ? commentWriter.attributes.name : ''
-    const body = !isEmpty(comment.attributes) ? comment.attributes.body : ''
-    const nameString = name ? `${name}:` : 'unknown:'
-
+const renderEntries = (entries) => {
+  if (isEmpty(entries)) {
     return (
-      <li key={`entry-${entry.id}-${comment.id}`}>
-        {`${nameString} ${body}`}
+      <div className={styles['blog-details-container']}>
+        <h4>No entries found.</h4>
+      </div>
+    )
+  }
+
+  return entries.map((entry) => {
+    const { author, attributes, comments } = entry
+  
+    const authorDetails = (
+      <div>
+        <h5>Author</h5>
+        <ul>
+          <li>{author.attributes.name}: {author.attributes.email}</li>
+        </ul>
+      </div>
+    )
+  
+    const attributeItems = Object.keys(attributes).map(key =>(
+      <li key={`entry-${entry.id}-key-${key}`}>
+        {key}: {entry.attributes[key]}
       </li>
+    ))
+  
+    const entryAttributes = (
+      <div>
+        <h5>Details</h5>
+        <ul>
+          {attributeItems}
+        </ul>
+      </div>
+    )
+  
+    const commentItems = comments.map((comment) => {
+      const { commentWriter } = comment
+      const name = !isEmpty(commentWriter) ? commentWriter.attributes.name : ''
+      const body = !isEmpty(comment.attributes) ? comment.attributes.body : ''
+      const nameString = name ? `${name}:` : 'unknown:'
+  
+      return (
+        <li key={`entry-${entry.id}-${comment.id}`}>
+          {`${nameString} ${body}`}
+        </li>
+      )
+    })
+    
+    return (
+      <Container key={`entry-container-${entry.id}`}>
+        <Row>
+          <Col>
+            <h5 className={styles['entry-header']}>Entry #{entry.id}</h5>
+            <div className={styles['blog-details-container']}>
+              {authorDetails}
+              {entryAttributes}
+              {commentItems}
+            </div>
+          </Col>
+        </Row>
+      </Container>
     )
   })
-  
-  return (
-    <Container key={`entry-container-${entry.id}`}>
-      <Row>
-        <Col>
-          <h5 className={styles['entry-header']}>Entry #{entry.id}</h5>
-          <div className={styles['blog-details-container']}>
-            {authorDetails}
-            {entryAttributes}
-            {commentItems}
-          </div>
-        </Col>
-      </Row>
-    </Container>
-  )
-})
+}
 
 function DetailView(props) {
   console.log(props)
